feaefdd6ef7abd774da5d027533a8d3995d5e412
Mihai Gaman <mihaidgaman@gmail.com>
2024-02-20 21:17:38 +0200

    glitch update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const baseUrl = import.meta.env.VITE_SERVER_URI;
+
 function App() {
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios
+      .get(`${baseUrl}/profile`)
+      .then((response) => {
+        const { data } = response;
+
+        setName(data.name);
+      })
+      .catch((response) => {
+        const { code } = response;
+
+        if (code === 'ERR_NETWORK') {
+          setMessage('Could not fetch data');
+        }
+      });
+  }, []);
+
+  if (message.trim().length > 0) {
+    return <div>{message}</div>;
+  }
+
   return (
     <>
-      <h1>Hello word</h1>
+      <h1 className="text-3xl font-bold underline">
+        {name.trim().length <= 0 ? '...loading' : name}
+      </h1>
     </>
   );
 }
